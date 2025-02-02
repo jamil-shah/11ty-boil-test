907f038f4e5a8e9c1e854779260c9667fd9392e2
jamil-shah <73444183+jamil-shah@users.noreply.github.com>
2025-02-02 21:13:49 +0000

    fix css

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
 
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("styles"); 
+  eleventyConfig.addShortcode("alert", function(message) {
+      return `<div class="alert">${message}</div>`;
+  });
+
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("styles");
+
   return {
       dir: {
           input: ".",
